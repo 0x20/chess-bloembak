f8e95b6bed273ca0411523969468b240913eef20
Mike Rosseel <mike.rosseel@gmail.com>
2023-01-09 00:08:05 +0100

    cleanup, tabs to spaces, better args

--- chess-bloembak.py
+++ chess-bloembak.py
@@ -30,52 +30,55 @@ king_arr = "0000011011110000"
 queen_arr = "0000011001100000"
 empty_arr = "0000000000000000"
 
-engine = chess.engine.SimpleEngine.popen_uci(r"/home/pi/miker/Stockfish-sf_15/src/stockfish")
+# engine = chess.engine.SimpleEngine.popen_uci(r"/home/pi/miker/Stockfish-sf_15/src/stockfish")
+engine = chess.engine.SimpleEngine.popen_uci(r"/opt/homebrew/bin/stockfish")
 engine.configure({"Threads": 4})
+
+
 class Canvas():
-	the_canvas = ""
-	CANVAS_LENGTH = 32*32*6
+    the_canvas = ""
+    CANVAS_LENGTH = 32*32*6
 
-	def __init__(self):
-		self.the_canvas = black*1024
-		assert len(self.the_canvas) == self.CANVAS_LENGTH
+    def __init__(self):
+        self.the_canvas = black*1024
+        assert len(self.the_canvas) == self.CANVAS_LENGTH
 
-	def pos_to_index(self, pos):
-		pos_in_row = pos%8
-		rows = math.floor(pos/8)
-		start = rows*768 + pos_in_row * 6*4
-		return start
+    def pos_to_index(self, pos):
+        pos_in_row = pos%8
+        rows = math.floor(pos/8)
+        start = rows*768 + pos_in_row * 6*4
+        return start
 
-	def set_piece(self, sub_frame, position):
-		#print(f"Setting piece with subframe {sub_frame} on pos {position}")
-		#self.print(frame=sub_frame, line=4)
-		start = self.pos_to_index(position)
-		#print(f"start is {start}")
-		for i in range(0, len(sub_frame), 24):
-			self.the_canvas = self.the_canvas[:start] + sub_frame[i:i+24] + self.the_canvas[start+24:]
-			#print(self.the_canvas[:start-1], " ", sub_frame[i:i+24], " ", self.the_canvas[start+24:])
-			#print(len(self.the_canvas))
-			assert len(self.the_canvas) == self.CANVAS_LENGTH, f"len is {len(self.the_canvas)}"
-			start = start + 6*32
+    def set_piece(self, sub_frame, position):
+        #print(f"Setting piece with subframe {sub_frame} on pos {position}")
+        #self.print(frame=sub_frame, line=4)
+        start = self.pos_to_index(position)
+        #print(f"start is {start}")
+        for i in range(0, len(sub_frame), 24):
+            self.the_canvas = self.the_canvas[:start] + sub_frame[i:i+24] + self.the_canvas[start+24:]
+            #print(self.the_canvas[:start-1], " ", sub_frame[i:i+24], " ", self.the_canvas[start+24:])
+            #print(len(self.the_canvas))
+            assert len(self.the_canvas) == self.CANVAS_LENGTH, f"len is {len(self.the_canvas)}"
+            start = start + 6*32
 
-	def print(self, frame=None, line=32):
-		count = 1
-		if frame == None:
-			frame = self.the_canvas
-		os.system('clear')
-		#print(chr(27) + "[2J")
-		for i in range(0, len(frame), 6):
-			r = frame[i:i+2]
-			g = frame[i+2:i+4]
-			b = frame[i+4:i+6]
-			print_pixel(r,g,b)
-			if count == line:
-				print("")
-				count = 0
-			count = count + 1
+    def print(self, frame=None, line=32):
+        count = 1
+        if frame == None:
+            frame = self.the_canvas
+        os.system('clear')
+        #print(chr(27) + "[2J")
+        for i in range(0, len(frame), 6):
+            r = frame[i:i+2]
+            g = frame[i+2:i+4]
+            b = frame[i+4:i+6]
+            print_pixel(r,g,b)
+            if count == line:
+                print("")
+                count = 0
+            count = count + 1
 
-	def lumos(self):
-		lumos.push(self.the_canvas, 1)
+    def lumos(self):
+        lumos.push(self.the_canvas, 1)
 
 
 
@@ -83,23 +86,22 @@ board = chess.Board()
 canvas = Canvas()
 
 def create_piece(arr, back_color, piece_color):
-	result = ""
-	assert len(arr) == 16
-	for entry in arr:
-		if entry == '0':
-			result = result + back_color
-		else:
-			result = result + piece_color
-	assert len(result) == 16*6
-	return result
+    result = ""
+    assert len(arr) == 16
+    for entry in arr:
+        if entry == '0':
+            result = result + back_color
+        else:
+            result = result + piece_color
+    assert len(result) == 16*6
+    return result
 
 def print_pixel(r,g,b):
-	rh = int("0x"+r, 16)
-	gh = int("0x"+g, 16)
-	bh = int("0x"+b, 16)
-	print("\x1B[48;2;" + f"{rh};{gh};{bh}m", ENDC, end="")
-	print("\x1B[48;2;" + f"{rh};{gh};{bh}m", ENDC, end="")
-
+    rh = int("0x"+r, 16)
+    gh = int("0x"+g, 16)
+    bh = int("0x"+b, 16)
+    print("\x1B[48;2;" + f"{rh};{gh};{bh}m", ENDC, end="")
+    print("\x1B[48;2;" + f"{rh};{gh};{bh}m", ENDC, end="")
 
 
 knight = create_piece(knight_arr, black, white_piece)
@@ -122,131 +124,118 @@ other_line_full = other_line * 4
 frame = (start_line_full + other_line_full) * 4
 
 def print_frame(frame, line=32):
-	#print(f"len frame is {len(frame)}")
-	count = 1
-	for i in range(0, len(frame), 6):
-		r = frame[i:i+2]
-		g = frame[i+2:i+4]
-		b = frame[i+4:i+6]
-		print_pixel(r,g,b)
-		if count == line:
-			print("")
-			count = 0
-		count = count + 1
+    #print(f"len frame is {len(frame)}")
+    count = 1
+    for i in range(0, len(frame), 6):
+        r = frame[i:i+2]
+        g = frame[i+2:i+4]
+        b = frame[i+4:i+6]
+        print_pixel(r,g,b)
+        if count == line:
+            print("")
+            count = 0
+        count = count + 1
 
 def get_board_frame(fen, black_board_color=black, white_board_color=white):
-	frame = ""
-	board_colors=[white_board_color, black_board_color]
-	board_counter = 0
-	board_fen = fen.split(' ')[0]
-	position = 0
-	#print(board_fen)
-	for cased_char in board_fen:
-		piece_color = white_piece if cased_char.isupper() else black_piece
-		board_color = board_colors[board_counter%2]
-		#print(f"board color: {board_color}, piece color: {piece_color}")
-		char = cased_char.lower()
-		result = ""
-		if char == 'r':
-			result = create_piece(rook_arr, board_color, piece_color)
-			canvas.set_piece(result, position)
-			board_counter = board_counter + 1
-			position = position + 1
-		elif char == 'n':
-			result = create_piece(knight_arr, board_color, piece_color)
-			canvas.set_piece(result, position)
-			board_counter = board_counter + 1
-			position = position + 1
-		elif char == 'b':
-			result = create_piece(bishop_arr, board_color, piece_color)
-			canvas.set_piece(result, position)
-			board_counter = board_counter + 1
-			position = position + 1
-		elif char == 'q':
-			result = create_piece(queen_arr, board_color, piece_color)
-			canvas.set_piece(result, position)
-			board_counter = board_counter + 1
-			position = position + 1
-		elif char == 'k':
-			result = create_piece(king_arr, board_color, piece_color)
-			canvas.set_piece(result, position)
-			board_counter = board_counter + 1
-			position = position + 1
-		elif char == 'p':
-			result = create_piece(pawn_arr, board_color, piece_color)
-			canvas.set_piece(result, position)
-			board_counter = board_counter + 1
-			position = position + 1
-		elif char == '/':
-			result = ""
-			board_counter = board_counter + 1
-		else:
-			#print("unrecognized char", char)
-			number = int(char)
-			for i in range(0,number):
-				board_color = board_colors[board_counter%2]
-				board_counter = board_counter + 1
-				result = create_piece(empty_arr, board_color, board_color)
-				canvas.set_piece(result, position)
-				position = position + 1
-
+    frame = ""
+    board_colors=[white_board_color, black_board_color]
+    board_counter = 0
+    board_fen = fen.split(' ')[0]
+    position = 0
+    #print(board_fen)
+    for cased_char in board_fen:
+        piece_color = white_piece if cased_char.isupper() else black_piece
+        board_color = board_colors[board_counter%2]
+        #print(f"board color: {board_color}, piece color: {piece_color}")
+        char = cased_char.lower()
+        result = ""
+        if char == 'r':
+            result = create_piece(rook_arr, board_color, piece_color)
+            canvas.set_piece(result, position)
+            board_counter = board_counter + 1
+            position = position + 1
+        elif char == 'n':
+            result = create_piece(knight_arr, board_color, piece_color)
+            canvas.set_piece(result, position)
+            board_counter = board_counter + 1
+            position = position + 1
+        elif char == 'b':
+            result = create_piece(bishop_arr, board_color, piece_color)
+            canvas.set_piece(result, position)
+            board_counter = board_counter + 1
+            position = position + 1
+        elif char == 'q':
+            result = create_piece(queen_arr, board_color, piece_color)
+            canvas.set_piece(result, position)
+            board_counter = board_counter + 1
+            position = position + 1
+        elif char == 'k':
+            result = create_piece(king_arr, board_color, piece_color)
+            canvas.set_piece(result, position)
+            board_counter = board_counter + 1
+            position = position + 1
+        elif char == 'p':
+            result = create_piece(pawn_arr, board_color, piece_color)
+            canvas.set_piece(result, position)
+            board_counter = board_counter + 1
+            position = position + 1
+        elif char == '/':
+            result = ""
+            board_counter = board_counter + 1
+        else:
+            #print("unrecognized char", char)
+            number = int(char)
+            for i in range(0,number):
+                board_color = board_colors[board_counter%2]
+                board_counter = board_counter + 1
+                result = create_piece(empty_arr, board_color, board_color)
+                canvas.set_piece(result, position)
+                position = position + 1
 
 
 def game_loop(console_output=False, delay=2):
-	if not console_output:
-		import lumos
-	output = canvas.print if console_output else canvas.lumos
-	output()
-	while 1:
-		#moves = list(board.legal_moves)
-		#move = moves[random.randint(0, len(moves)-1)]
-		result = engine.play(board, chess.engine.Limit(time=0.1))
-		board.push(result.move)
-		get_board_frame(board.fen())
-		output()
-		# blink the board 5 times if the game is finished
-		if board.outcome() is not None:
-			print("board outcome is ", board.outcome())
-			for i in range(0,5):
-				get_board_frame(board.fen(), black_board_color=white, white_board_color=black)
-				output()
-				time.sleep(delay/4)
-				get_board_frame(board.fen())
-				output()
-				time.sleep(delay/4)
-			time.sleep(delay*10)
-			board.reset()
-		time.sleep(delay)
+    if not console_output:
+        import lumos
+    output = canvas.print if console_output else canvas.lumos
+    output()
+    while 1:
+        #moves = list(board.legal_moves)
+        #move = moves[random.randint(0, len(moves)-1)]
+        result = engine.play(board, chess.engine.Limit(time=0.1))
+        board.push(result.move)
+        get_board_frame(board.fen())
+        output()
+        # blink the board 5 times if the game is finished
+        if board.outcome() is not None:
+            print("board outcome is ", board.outcome())
+            for i in range(0,5):
+                get_board_frame(board.fen(), black_board_color=white, white_board_color=black)
+                output()
+                time.sleep(delay/4)
+                get_board_frame(board.fen())
+                output()
+                time.sleep(delay/4)
+            time.sleep(delay*10)
+            board.reset()
+        time.sleep(delay)
 
 
 if __name__ == "__main__":
-	logger = logging.getLogger()
-	logger.setLevel(logging.INFO)
-	logging.basicConfig(format="%(asctime)s %(name)s: %(levelname)s %(message)s")
-	parser = argparse.ArgumentParser(description="munipack automation cli")
-	parser.add_argument(
-		"--ascii",
-		help="outputs the frames on the terminal",
-		action="store_true",
-		default=False,
-	)
-	parser.add_argument(
-		"-x", "--verbose", help="Set logging to debug mode", action="store_true"
-	)
-	parser.add_argument(
-		"-r",
-		"--resultdir",
-		help="The directory where all results will be written",
-		nargs="?",
-		required=True,
-	)
-	parser.add_argument(
-		"--fitsdir",
-		help="The dir where the fits are, only needed to plate-solve the reference frame",
-		required=False,
-	)
-	args = parser.parse_args()
-	if args.verbose:
-			logger.setLevel(logging.DEBUG)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logging.basicConfig(format="%(asctime)s %(name)s: %(levelname)s %(message)s")
+    parser = argparse.ArgumentParser(description="chess on bloembak")
+    parser.add_argument(
+        "--ascii",
+        help="outputs the frames on the terminal",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "-x", "--verbose", help="Set logging to debug mode", action="store_true"
+    )
+    args = parser.parse_args()
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
 
-	game_loop(console_output=args.ascii, delay=10)
+    game_loop(console_output=args.ascii, delay=10)
